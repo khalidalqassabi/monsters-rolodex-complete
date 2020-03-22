8a32552bc5c542e28cb45fa307cd252e7df95811
khalidalqassabi <khq94k@gmail.com>
2020-03-22 16:46:45 +0400

    Updates

---
+++ precache-manifest.4741ac83628fe75711f614b16296c58c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d41f5468d4b525a2aa4f9911833b73",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "986a17317f76dd07780c",
+    "url": "/monsters-rolodex-complete/static/css/main.539b450d.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/monsters-rolodex-complete/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "986a17317f76dd07780c",
+    "url": "/monsters-rolodex-complete/static/js/main.44c754e7.chunk.js"
+  },
+  {
+    "revision": "683efa6afd61321d8334",
+    "url": "/monsters-rolodex-complete/static/js/runtime~main.0e367587.js"
+  }
+]);
